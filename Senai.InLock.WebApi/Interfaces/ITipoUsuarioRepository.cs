8be7fedc08c875e6f579ff5066b081d0200bf320
Gabriel Ferreira Leite Da Silva <50738510807@info.local>
2020-03-02 17:25:54 -0300

    first commit

---
+++ Senai.InLock.WebApi/Interfaces/ITipoUsuarioRepository.cs
@@ -0,0 +1,23 @@
+﻿using Senai.InLock.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Interfaces
+{
+    interface ITipoUsuarioRepository
+    {
+        List<TipoUsuarioDomain> Listar();
+
+        TipoUsuarioDomain BuscarPorId(int id);
+
+        void Cadastrar(TipoUsuarioDomain novoTipoUsuario);
+
+        void Atualizar(int id, TipoUsuarioDomain tipoUsuarioAtualizado);
+
+        void Deletar(int id);
+
+
+    }
+}
---
+++ Senai.InLock.WebApi/Repositories/TipoUsuarioRepository.cs
@@ -0,0 +1,37 @@
+﻿using Senai.InLock.WebApi.Domains;
+using Senai.InLock.WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Repositories
+{
+    public class TipoUsuarioRepository : ITipoUsuarioRepository
+    {
+        public void Atualizar(int id, TipoUsuarioDomain tipoUsuarioAtualizado)
+        {
+            
+        }
+
+        public TipoUsuarioDomain BuscarPorId(int id)
+        {
+            
+        }
+
+        public void Cadastrar(TipoUsuarioDomain novoTipoUsuario)
+        {
+           
+        }
+
+        public void Deletar(int id)
+        {
+            
+        }
+
+        public List<TipoUsuarioDomain> Listar()
+        {
+            
+        }
+    }
+}
